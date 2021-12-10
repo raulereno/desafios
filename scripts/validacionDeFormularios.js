9378d1b64d3c2a1afc5f98e6af8ccd49fbc47be8
raulereno <raulereno@gmail.com>
2021-12-10 01:31:14 -0300

    comentando codigo no utilizado todavia

--- scripts/validacionDeFormularios.js
+++ scripts/validacionDeFormularios.js
@@ -1,12 +1,12 @@
-let campoUsuario = document.getElementById("campoUsuario");
-let campoContrasenia =document.getElementById("campoContrasenia");
-let bodyModal = document.getElementById("bodyModal")
+// let campoUsuario = document.getElementById("campoUsuario");
+// let campoContrasenia =document.getElementById("campoContrasenia");
+// let bodyModal = document.getElementById("bodyModal")
 
-let enviarIngreso= document.getElementById("formularioIngreso");
-enviarIngreso.addEventListener("submit",validarFormulario);
+// let enviarIngreso= document.getElementById("formularioIngreso");
+// enviarIngreso.addEventListener("submit",validarFormulario);
 
-function validarFormulario(e){
-    e.preventDefault();
-    console.log("formulario enviado")
+// function validarFormulario(e){
+//     e.preventDefault();
+//     console.log("formulario enviado")
   
-}
+// }
