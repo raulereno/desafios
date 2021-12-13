6bf49c7c41ebc43473c32673cb6b63be93809c28
raulereno <raulereno@gmail.com>
2021-12-13 15:56:15 -0300

    desafio jquery completo, falta persistencia de la lista del carrito

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,39 +1,65 @@
+//Defino variable carrito y la inicio como array vacio 
 let carrito =[];
+console.log(carrito);
+//Variables que toman elementos del DOM
+let botonFinalizar = $("#finalizarCompra").click((e) => {finalizarCompra();})
+let montoTotalAPagar = $("#montoTotal");
+let cantidadDeProductos = $("#cantProductos");
+let cantidadDeProductos2= $("#prodTotalesVentana");
 
-let botonFinalizar = document.getElementById("finalizarCompra").onclick = () => finalizarCompra();
-let montoTotalAPagar = document.getElementById("montoTotal");
-let cantidadDeProductos = document.getElementById("cantProductos");
-let cantidadDeProductos2= document.getElementById("prodTotalesVentana");
+let cantidad=0;
 
-if (localStorage.length > 0) { 
+//Comprobacion para ver si el localStorage tiene algun producto guardado
+if (localStorage.getItem("carritoPersistencia") != null) { 
     carrito=JSON.parse(localStorage.getItem("carritoPersistencia"));
     calcularTotal()
 }
-console.log(carrito);
 
-
-function agregarAlCarro(id){
-    carrito.push(listaDeProductos[id-1]);
-    console.log(carrito);
-    localStorage.setItem("carritoPersistencia",JSON.stringify(carrito));
-    calcularTotal();    
+//Funcion que agrega productos al carrito, es llamada desde el archivo scripts linea 72. 
+function agregarAlCarro(producto){
+    let encontrado = carrito.find( e=>e.codigo == producto.codigo);
+    console.log(encontrado);
+    cantidad++;
+    
+    if(encontrado == undefined){
+        carrito.push(producto);
+        console.log(carrito);
+        localStorage.setItem("carritoPersistencia",JSON.stringify(carrito));
+        
+        $("#tablaBody").append(`
+        <tr id="fila${carrito.codigo}"> 
+        <th scope="row"><img src="../images/${producto.nombreFoto}" class="imgCarrito"></th> 
+        <td>${producto.nombre}</td> 
+        <td>$${producto.precio}</td> 
+        <td id="cantidad${producto.codigo}">${producto.cantidad}</td> 
+        <td><button class="btn btn-light"><img src="../images/trash.png" class="basureroCarrito"></button></td> </tr>`);
+    }
+    else{
+        let posicion = carrito.findIndex(e => e.codigo == producto.codigo);
+        console.log(posicion);
+        carrito[posicion].cantidad +=1;
+        $(`#cantidad${producto.codigo}`).html(carrito[posicion].cantidad);
+    }  
+    montoTotalAPagar.html(`$${calcularTotal()}`)    
+    cantidadDeProductos.html (cantidad);
+    cantidadDeProductos2.html(cantidad);
+    console.log(cantidad);
 }
 
-
+//Función que calcula el total de los precios
 function calcularTotal(){
     let total = 0;
     for(let prodEnCarro of carrito){
-        total += prodEnCarro.precio
+        total += (prodEnCarro.precio*prodEnCarro.cantidad);
         console.log(total)
-    }
-    montoTotalAPagar.innerText ="$" + total;
-    cantidadDeProductos.innerText = carrito.length;
-    cantidadDeProductos2.innerText= carrito.length;
+    } 
+    return total;
 }
-
+//Funcion que limpia el localStorage y reestablece los valores de precio total, y cantidad de productos
 function finalizarCompra(){
     localStorage.clear();
-    montoTotalAPagar.innerText ="$" + 0;
-    cantidadDeProductos.innerText = 0;
-    cantidadDeProductos2.innerText= 0;
+    montoTotalAPagar.html ("$" + 0);
+    cantidadDeProductos.html ( 0 );
+    cantidadDeProductos2.html( 0 );
+    cantidad=0;
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,4 @@
-//Creacion de la clase producto
-class Producto{
-    constructor(nombre,precio,stock,codigo){
-        this.nombre = nombre;
-        this.precio = parseFloat(precio);
-        this.stock = parseInt(stock);
-        this.codigo = parseInt(codigo)
-    }
-}
+
 //-------------------Funciones:---------------------
 //Funcion que mediante el numero elegido devuelve el precio del producto
 function precioInventario(numeroDeProducto){
@@ -34,40 +26,4 @@ function calcularInteres(precio, cantidadDeCuotas){
         return precio + x;
     }
 }
-//Lista de productos
-const listaDeProductos = []; 
 
-let divPadreCards = document.getElementById("cards_productos");
-//Creacion de lo productos
-listaDeProductos.push(new Producto("Pelota de Goma",300,6,1));
-listaDeProductos.push(new Producto("Cepillo de Dientes",150,10,2));
-listaDeProductos.push(new Producto("Hueso Grande",250,8,3));
-listaDeProductos.push(new Producto("Shampoo Antiparasitario",350,10,4));
-listaDeProductos.push(new Producto("Corta Uñas",500,5,0005));
-listaDeProductos.push(new Producto("Collar con Cascabel",400,11,6));
-listaDeProductos.push(new Producto("Espuma Seca",550,6,7));
-listaDeProductos.push(new Producto("Cepillo para pelos",550,15,8));
-listaDeProductos.push(new Producto("Comederos",280,18,9));
-
-console.log(listaDeProductos)
-
-//For...of que recorre la lista de objetos y va creando las cards correspondientes
-for(const producto of listaDeProductos){
-    let card = document.createElement("div");
-    //Agregando atributos al nuevo elemento
-    card.setAttribute("class", "card");
-    card.setAttribute("style","width: 15rem;");
-    //Utilizacion de plantillas literales para crear la estructura interna que va a tener el elemento
-    card.innerHTML =
-    ` <img src="../images/${producto.codigo}.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">Precio:$${producto.precio}<br>Stock: ${producto.stock}</p>
-        <button class="btn btn-primary" id="${producto.codigo}">Agregar al carrito</button>
-    </div>`;
-
-    //Asignacion del nodo hijo al nodo padre
-    divPadreCards.appendChild(card);
-    //Tomo el elemento por codigo y le ponemos un evento onClick para agregar al carro
-    document.getElementById(`${producto.codigo}`).onclick = () => agregarAlCarro(`${producto.codigo}`);
-}
